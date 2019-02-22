01b32ae2a1c0e2683b7231119d05b16ac64afe19
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-22 21:55:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles' ] ;
 function addElementToBeginningOfArray (arr, el) {
   return [ el, ...arr ] ;
 }
+
+function destructivelyAddElementToBeginningOfArray (arr, el) {
+  return arr.unshift(el) ;
+}
