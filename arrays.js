611576ab6de77e31df334a82d7d9e229ffa7a265
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-22 22:03:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,18 @@ function destructivelyRemoveElementFromBeginningOfArray(arr) {
   arr.shift();
   return arr;
 }
+
+function removeElementFromBeginningOfArray(arr) {
+  return arr.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  return arr.slice(0, arr.length - 1);
+}
+
+
